5916fd8e30cf276ae0f88f2cab74da5097525992
João Seródio <joaoserodio@live.com>
2024-08-15 19:28:34 -0300

    update recipes to use new TVM version (FloatImm fixed)

--- build/recipe_base.py
+++ build/recipe_base.py
@@ -1,4 +1,4 @@
-tvm_version = USERARG.get("tvm-version", "dev0.14.1")
+tvm_version = USERARG.get("tvm-version", "dev0.14.2")
 jax_version = USERARG.get("jax-version", "0.4.23")
 numpy_version = USERARG.get("numpy-version", "1.24.3")
 scipy_version = USERARG.get("scipy-version", "1.10.1")
@@ -48,11 +48,11 @@ Stage0 += shell(
     commands=[
         "pip install -U --pre cupy-cuda12x -f https://pip.cupy.dev/pre",
         f'''pip install scipy=={scipy_version} numpy=={numpy_version} jupyter \
-                        dacite dask dask_cuda dask_jobqueue dask_memusage GPUtil \
-                        gdown graphviz h5py ipympl matplotlib memray networkx ormsgpack packaging \
-                        portalocker psutil pyarrow xarray scikit-image \
-                        xgboost zarr glcm-cupy multidimio segyio segysak py-cpuinfo \
-                        bokeh==2.4.3 \"protobuf<=3.20.1\" \"charset-normalizer<3.0\" \"tornado<6.2\"''',
+                        dacite==1.8.1 dask==2023.3.2 dask_cuda==23.6.0 dask_jobqueue==0.8.2 dask_memusage==1.1 GPUtil==1.4.0 \
+                        gdown==4.7.1 graphviz==0.20.1 h5py==3.10.0 ipympl==0.9.3 matplotlib==3.7.2 memray==1.11.0 networkx==3.2.1 ormsgpack==1.4.1 packaging==23.1 \
+                        portalocker==2.8.2 psutil==5.9.4 pyarrow==11.0.0 xarray==2023.12.0 scikit-image==0.22.0 \
+                        xgboost==1.7.5 zarr==2.16.1 glcm-cupy==0.2.1 multidimio==0.4.2 segyio==1.9.12 segysak==0.3.4 py-cpuinfo==9.0.0 \
+                        bokeh==2.4.3 protobuf==3.20.1 charset-normalizer==2.1.1 tornado==6.1''',
         f"pip install -U \"jax[cuda12_pip]=={jax_version}\" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html"
     ]
 )
